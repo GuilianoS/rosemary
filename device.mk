64a651b6324caaee73995bc601673604a786de06
GuilianoS <guiliano-sfalcin@hotmail.com>
2024-01-08 10:31:33 -0300

    rosemary: fix bluetooth audio

--- device.mk
+++ device.mk
@@ -144,6 +144,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     libbluetooth_audio_session
 
 #CAS
